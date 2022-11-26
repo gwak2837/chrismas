f64b5ba7669a19bfeee349420e3a84d635dd83c4
Taeuk Gwak <gwak2837@kakao.com>
2022-11-26 13:37:33 +0900

    Connect to database 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
 import Image from 'next/image'
-import { useQuery } from 'react-query'
 
+import { SERVER_URL } from '../common/constants'
 import styles from './page.module.css'
 
 async function getData() {
-  const res = await fetch('/api', { cache: 'no-store' })
+  const res = await fetch(SERVER_URL + '/api', { cache: 'no-store' })
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
   }
 
   return res.json()
 }
 
-export default function Home() {
+export default async function Home() {
   const data = await getData()
 
   return (
     <main>
       <h1>크리스마스 선물 주기</h1>
-      <div>데이터베이스 서버 시간: {data}</div>
+      <div>데이터베이스 서버 시간: {data.time}</div>
     </main>
   )
 }
--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,2 @@
 export const POSTGRES_CA = process.env.POSTGRES_CA
+export const SERVER_URL = process.env.SERVER_URL
